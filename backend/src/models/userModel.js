27d54fac5b10112339f857980103a5b5fb58fe94
xyver <xyvertucio@gmail.com>
2026-03-24 23:58:15 +0800

    Update Variable for createUser model

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -2,25 +2,25 @@ const db = require("../config/db.js").promise();
 
 const User = {
   // Insert new user into database and return user with data with new ID
-  createUser: async (user) => {
+  createUser: async (newUser) => {
     try {
       const sql = `INSERT INTO admin (phone_number, last_name, first_name, middle_initial, username, password, is_active)
                  VALUES (?, ?, ?, ?, ?, ?, 1)`;
 
       const [result] = await db.execute(sql, [
-        user.phone_number,
-        user.last_name,
-        user.first_name,
-        user.middle_initial || null, // Optional Middle Initial, set to null if not provided
-        user.username,
-        user.password, // Hashed on controller before saving
+        newUser.phone_number,
+        newUser.last_name,
+        newUser.first_name,
+        newUser.middle_initial || null, // Optional Middle Initial, set to null if not provided
+        newUser.username,
+        newUser.password, // Hashed on controller before saving
       ]);
       return { admin_id: result.insertId, ...user }; // Return the newly created driver's ID and inserted a unique ID via auto-increment in the database
     } catch (error) {
       throw new Error("Failed to create user: " + error);
     }
   },
-  //
+  // Get user by username, return user if found, otherwise return null
   getUserByUsername: async (username) => {
     try {
       // Select user by username and is_active = 1, return only one user if found
